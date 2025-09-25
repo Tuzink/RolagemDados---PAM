cf2c413f15e3c4659326272a31e169a5d853714d
Tuzink <mirandaartur1230@gamil.com>
2025-09-24 22:31:08 -0300

    a

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace RoladorDeDadosMAUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnRolarDadoClicked(object sender, EventArgs e)
+    {
+        if (LadosPicker.SelectedItem is int lados)
+        {
+            Random random = new Random();
+            int resultado = random.Next(lados + 1);
+            ResultadoLabel.Text = resultado.ToString();
+        }
+        else
+        {
+            ResultadoLabel.Text = "⚠️";
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RoladorDeDadosMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
